d891436d657e4426a408228e210827165585d6e9
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2021-07-25 12:41:40 +0530

    updt: reformatted

--- ratrix.py
+++ ratrix.py
@@ -1,4 +1,4 @@
-import requests # external import
+import requests  # external import
 import urllib
 import sys
 import os
@@ -7,14 +7,16 @@ from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
 API_KEY = None
 
+
 def get_api_key():
-    if os.path.isfile(".apikey") == False:
+    if os.path.isfile(".apikey") is False:
         print("Error: Unable to obtain the API key!")
         print("Obtain an API key omdb.com and save in .apikey inside present folder!")
         sys.exit(0)
     global API_KEY
     with open(".apikey", "r") as f:
         API_KEY = f.read().strip()
+
 
 def build_request(**kwargs):
     global API_KEY
@@ -23,12 +25,14 @@ def build_request(**kwargs):
         req += "&" + arg + "=" + urllib.parse.quote(str(kwargs[arg]), safe='')
     return requests.get(req)
 
+
 def search(name):
     global API_KEY
     r = build_request(t=name)
     if r.status_code == 404 or r.json()["Response"] == "False":
         return None
     return r.json()
+
 
 def get_season_details(response):
     num_seasons = int(response["totalSeasons"])
@@ -49,18 +53,21 @@ def get_season_details(response):
     print(" Done")
     return season_details
 
+
 def get_episode_details(season_details):
     episode_details = []
     for season in season_details:
-        if season == None:
+        if season is None:
             episode_details.append([])
             continue
         season_wise_episodes = []
         episodes = season["Episodes"]
         for episode in episodes:
-            season_wise_episodes.append((episode["Title"], episode["Episode"], episode["imdbRating"]))
+            season_wise_episodes.append(
+                (episode["Title"], episode["Episode"], episode["imdbRating"]))
         episode_details.append(season_wise_episodes)
     return episode_details
+
 
 def hex_to_rgb(h):
     return tuple(int(h.lstrip("#")[i:i+2], 16) for i in (0, 2, 4))
@@ -68,10 +75,13 @@ def hex_to_rgb(h):
 # colors should follow the sequence:
 # background, foreground, header, worst, good, best, N/A
 
+
 # gruvbox colors
-GRUVBOX = ["#282828", "#ebdbb2", "#458588", "#cc241d", "#d79921", "#98971a", "#8ec07c"]
+GRUVBOX = ["#282828", "#ebdbb2", "#458588",
+           "#cc241d", "#d79921", "#98971a", "#8ec07c"]
 # elementary colors
-ELEMENTARY = ["#101010", "#f2f2f2", "#101010", "#e1321a", "#ffc005", "#6ab017", "#2aa7e7"]
+ELEMENTARY = ["#101010", "#f2f2f2", "#101010",
+              "#e1321a", "#ffc005", "#6ab017", "#2aa7e7"]
 
 # 0 -> transparent, 1 -> opaque
 OPACITY = 0.85
@@ -99,6 +109,7 @@ STAT_PADDING = 30
 # denotes the spacing between title and overall rating
 NAME_RATING_SPACING = 10
 
+
 def calculate_index_by_grade(grade):
     # bad < 4.0
     # medium < 8.0
@@ -109,6 +120,7 @@ def calculate_index_by_grade(grade):
     if grade > 7.9:
         index += 1
     return index
+
 
 def draw_row(im, start, text, grades=False):
     global BOX_HEIGHT, BOX_WIDTH, FONT
@@ -126,9 +138,10 @@ def draw_row(im, start, text, grades=False):
         right = left + BOX_WIDTH
         bottom = top + BOX_HEIGHT
         current_left = right
-        if cell == None:
+        if cell is None:
             continue
-        rect_dim = (left, top + (MARKER_PADDING_HEIGHT / 2), left + MARKER_WIDTH, bottom - (MARKER_PADDING_HEIGHT / 2))
+        rect_dim = (left, top + (MARKER_PADDING_HEIGHT / 2), left +
+                    MARKER_WIDTH, bottom - (MARKER_PADDING_HEIGHT / 2))
         rect_color = COLORS[0]
         # 1) we don't color the first cell, because it is the row header.
         # 2) for N/A cells, we set to a specific color
@@ -142,26 +155,31 @@ def draw_row(im, start, text, grades=False):
         textsize = im.textsize(cell, font=FONT)
         pad_left = (BOX_WIDTH - textsize[0] + MARKER_WIDTH) / 2
         pad_top = (BOX_HEIGHT - textsize[1]) / 2
-        im.text((left + pad_left, top + pad_top), cell, fill=COLORS[1], font=FONT)
+        im.text((left + pad_left, top + pad_top),
+                cell, fill=COLORS[1], font=FONT)
         i += 1
+
 
 def calculate_text_size(text, fnt):
     return fnt.getsize(text)
+
 
 def calculate_stat_size(name, without_overall=False):
     size = calculate_text_size(name, TITLEFONT)
     name_rate_spacing = NAME_RATING_SPACING
     if without_overall:
-        ratings = (0,0)
+        ratings = (0, 0)
         name_rate_spacing = 0
     else:
         ratings = calculate_text_size("10.0", FONT)
     return (size[0] + ratings[0] + name_rate_spacing + (STAT_PADDING * 2), size[1] + STAT_SPACING)
 
+
 def calculate_ratings_size(name, num_seasons, max_ep_in_a_season):
     global BOX_HEIGHT, BOX_WIDTH, PADDING
-    min_height = BOX_HEIGHT * (max_ep_in_a_season + 1) # we need to draw the season row
-    min_width = BOX_WIDTH * (num_seasons + 1) # we need to draw ep column
+    # we need to draw the season row
+    min_height = BOX_HEIGHT * (max_ep_in_a_season + 1)
+    min_width = BOX_WIDTH * (num_seasons + 1)  # we need to draw ep column
     height = min_height + (PADDING * 2)
     width = min_width + (PADDING * 2)
     stat_width, stat_height = calculate_stat_size(name)
@@ -172,6 +190,7 @@ def calculate_ratings_size(name, num_seasons, max_ep_in_a_season):
     height += stat_height
     return width, height
 
+
 def calculate_size(posterim, name, num_seasons, max_ep_in_a_season):
     # given a minimum size to draw the text,
     # this function resizes the poster or the
@@ -180,7 +199,8 @@ def calculate_size(posterim, name, num_seasons, max_ep_in_a_season):
     # the BOX dims are modified. If the content
     # size is larger, then the image is resized.
     global BOX_WIDTH, BOX_HEIGHT, PADDING
-    min_width, min_height = calculate_ratings_size(name, num_seasons, max_ep_in_a_season)
+    min_width, min_height = calculate_ratings_size(
+        name, num_seasons, max_ep_in_a_season)
     img_width, img_height = posterim.size
     if min_width > img_width:
         img_width = min_width
@@ -190,7 +210,8 @@ def calculate_size(posterim, name, num_seasons, max_ep_in_a_season):
             BOX_WIDTH += 1
             BOX_HEIGHT += 1
             PADDING += 1
-            min_width, min_height = calculate_ratings_size(name, num_seasons, max_ep_in_a_season)
+            min_width, min_height = calculate_ratings_size(
+                name, num_seasons, max_ep_in_a_season)
         img_width = min_width
     if min_height > img_height:
         img_height = min_height
@@ -199,10 +220,12 @@ def calculate_size(posterim, name, num_seasons, max_ep_in_a_season):
             BOX_HEIGHT += 1
             BOX_WIDTH += 1
             PADDING += 1
-            min_width, min_height = calculate_ratings_size(name, num_seasons, max_ep_in_a_season)
+            min_width, min_height = calculate_ratings_size(
+                name, num_seasons, max_ep_in_a_season)
         img_width = min_width
         img_height = min_height
     return min_width, min_height
+
 
 def generate_image(show, episode_details, filename):
     global BOX_WIDTH, BOX_HEIGHT, PADDING, FONT
@@ -213,7 +236,8 @@ def generate_image(show, episode_details, filename):
     print("Downloading poster..")
     poster = Image.open(urllib.request.urlopen(show["Poster"]))
     print("Adjusting the image dimensions..")
-    width, height = calculate_size(poster, show["Title"], num_seasons, max_ep_in_a_season)
+    width, height = calculate_size(
+        poster, show["Title"], num_seasons, max_ep_in_a_season)
     # resize and blur the poster
     print("Resizing and blurring the poster..")
     poster = poster.resize((width, height))
@@ -225,16 +249,20 @@ def generate_image(show, episode_details, filename):
     # draw the row column headers
     print("Drawing seasons and episode headers..")
     column_header_size = calculate_text_size("Seasons", FONT)
-    draw.text((((width - column_header_size[0]) / 2) + (BOX_WIDTH / 2), PADDING - column_header_size[1]), "Seasons", fill=COLORS[1], font=FONT)
+    draw.text((((width - column_header_size[0]) / 2) + (BOX_WIDTH / 2),
+              PADDING - column_header_size[1]), "Seasons", fill=COLORS[1], font=FONT)
     row_header_size = calculate_text_size("Episodes", FONT)
-    row_header_img = Image.new('RGB', (row_header_size[0], row_header_size[0]), COLORS[0])
+    row_header_img = Image.new(
+        'RGB', (row_header_size[0], row_header_size[0]), COLORS[0])
     row_header_draw = ImageDraw.Draw(row_header_img)
     row_header_draw.text((0, 0), "Episodes", fill=COLORS[1], font=FONT)
     row_header_pos_left = row_header_size[0] - row_header_size[1]
     row_header_img = row_header_img.rotate(270)
-    row_header_img = row_header_img.crop((row_header_pos_left, 0, row_header_pos_left + row_header_size[1], row_header_size[0]))
+    row_header_img = row_header_img.crop(
+        (row_header_pos_left, 0, row_header_pos_left + row_header_size[1], row_header_size[0]))
     row_header_img = row_header_img.rotate(180)
-    im.paste(row_header_img, ((PADDING - row_header_size[1]), int((height - row_header_size[0])/ 2)))
+    im.paste(row_header_img, ((
+        PADDING - row_header_size[1]), int((height - row_header_size[0]) / 2)))
     # draw the seasons header
     seasons_header = [i for i in range(1, len(episode_details) + 1)]
     seasons_header.insert(0, None)
@@ -256,21 +284,25 @@ def generate_image(show, episode_details, filename):
     # write show name
     name_width, name_height = calculate_text_size(show["Title"], TITLEFONT)
     rating_width, rating_height = calculate_text_size(show["imdbRating"], FONT)
-    draw.text((STAT_PADDING, height - name_height - STAT_PADDING), show["Title"], fill=COLORS[1], font=TITLEFONT)
+    draw.text((STAT_PADDING, height - name_height - STAT_PADDING),
+              show["Title"], fill=COLORS[1], font=TITLEFONT)
 
     orate_left = STAT_PADDING + name_width + NAME_RATING_SPACING
     orate_top = height - name_height - STAT_PADDING
     orate_bottom = orate_top + rating_height
     orate_right = orate_left + (MARKER_WIDTH / 2)
     orate = show["imdbRating"]
-    draw.rectangle((orate_left, orate_top, orate_right, orate_bottom), fill=COLORS[calculate_index_by_grade(float(orate))])
-    draw.text((orate_right + 5, orate_top), show["imdbRating"], fill=COLORS[1], font=FONT)
+    draw.rectangle((orate_left, orate_top, orate_right, orate_bottom),
+                   fill=COLORS[calculate_index_by_grade(float(orate))])
+    draw.text((orate_right + 5, orate_top),
+              show["imdbRating"], fill=COLORS[1], font=FONT)
 
     print("Blending with the poster..")
     finalimage = Image.blend(poster, im, OPACITY)
     print("Saving the resulting image..")
     finalimage.save(filename)
     print("Completed successfully!")
+
 
 def main():
     if len(sys.argv) < 3:
@@ -279,7 +311,7 @@ def main():
     get_api_key()
     print("Searching for '%s'.." % sys.argv[1])
     show = search(sys.argv[1])
-    if show != None:
+    if show is not None:
         try:
             print("Gathering information about '%s'.." % show["Title"])
             seasons = get_season_details(show)
@@ -291,6 +323,7 @@ def main():
             print("Unable to generate the matrix!")
     else:
         print("Error: Unable to get details about '%s'!" % sys.argv[1])
+
 
 if __name__ == "__main__":
     main()
